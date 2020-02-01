33093fac834d3917d9f79db6942a702226f9343f
hrnugr0000 <ugur.harun@yandex.com>
2020-02-01 19:34:50 +0300

    Updates

---
+++ precache-manifest.7d7b2e9b56cec485d85df24842fef113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04d3c5894e9de8e434434ffb793f677",
+    "url": "/basic-clock/index.html"
+  },
+  {
+    "revision": "82d01cce144f55331666",
+    "url": "/basic-clock/static/css/main.069a9cac.chunk.css"
+  },
+  {
+    "revision": "ea2557dea02ccbb7699d",
+    "url": "/basic-clock/static/js/2.d8699279.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/basic-clock/static/js/2.d8699279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d01cce144f55331666",
+    "url": "/basic-clock/static/js/main.9483ada7.chunk.js"
+  },
+  {
+    "revision": "48240667857c09e27ff9",
+    "url": "/basic-clock/static/js/runtime-main.e4d201cb.js"
+  }
+]);
